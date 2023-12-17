6c39f1641a3eab875a1fa40af5839604de9a3b37
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2023-12-18 00:03:36 +0100

    refactor: Added eslint rule overrides
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,31 @@
-import eslintConfig from "@worldofsoftware/configs/eslint";
+import eslintConfig, { tsOverrides } from "@worldofsoftware/configs/eslint";
 
-export default eslintConfig;
+export default [
+  ...eslintConfig,
+  {
+    files: ["packages/OpenProjectPlanner-API/**/*.ts"],
+    rules: {
+      "new-cap": ["warn", { capIsNew: false }],
+      "@typescript-eslint/no-extraneous-class": [
+        "error",
+        { allowWithDecorator: true }
+      ]
+    }
+  },
+  {
+    files: ["packages/OpenProjectPlanner-API/src/lib/env.ts"],
+    rules: {
+      "@typescript-eslint/naming-convention": [
+        "error",
+        ...tsOverrides.namingConvention,
+        {
+          selector: "objectLiteralProperty",
+
+          format: ["camelCase", "UPPER_CASE"],
+          leadingUnderscore: "forbid",
+          trailingUnderscore: "forbid"
+        }
+      ]
+    }
+  }
+];
