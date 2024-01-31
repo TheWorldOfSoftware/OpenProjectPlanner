f694276560fe69f95f6246df0efc5dd2cd90437d
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2024-01-31 22:48:54 +0100

    refactor: Got config working for OpenProjectPlanner
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,21 @@
 import eslintConfig from "@worldofsoftware/configs/eslint";
+import typescriptParser from "@typescript-eslint/parser";
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
 
 export default [
   ...eslintConfig,
   {
     files: ["packages/OpenProjectPlanner-API/**/*.ts"],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parser: typescriptParser
+    },
+    plugins: {
+      "@typescript-eslint": typescriptPlugin
+    },
     rules: {
       "new-cap": ["warn", { capIsNew: false }],
       "@typescript-eslint/no-extraneous-class": [
